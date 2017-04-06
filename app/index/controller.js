9ba0d110d5866931cc436fcc9b89b5b680ae5641
Nathan Vranicar <nathan@Nathans-MacBook-Pro-4.local>
2017-04-06 14:58:01 -0500

    added mock data and map zoom functionality

--- app/index/controller.js
+++ app/index/controller.js
@@ -1,9 +1,19 @@
 import Ember from 'ember';
+import data from './data';
 
 export default Ember.Controller.extend({
   lat: 38.82259097617713,
   lng: 9.140625000000002,
   zoom: 2,
   topo: true,
-  ironYard: [36.152706, -86.776111]
+  ironYard: [36.152706, -86.776111],
+  data,
+
+  actions: {
+    zoomByLoc(trip) {
+      Ember.set(this, 'lat', trip.crd[0]);
+      Ember.set(this, 'lng', trip.crd[1]);
+      Ember.set(this, 'zoom', 5);
+    }
+  }
 });
