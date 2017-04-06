d97853337bb119e45999a6aaf63cab94aba1d83a
Nathan Vranicar <nathan@Nathans-MacBook-Pro-4.local>
2017-04-06 14:58:01 -0500

    added mock data and map zoom functionality

---
+++ app/index/data.js
@@ -0,0 +1,29 @@
+export default {
+  "trips": [
+    {
+      "cover": "https://www.oneandonlyresorts.com/-/media/oneandonly/reethi-rah/about/793x634/one-and-only-reethi-rah-maldives-location.jpg?v1",
+      "location": "Maldives",
+      "user": "Nathan",
+      "crd": [1.924992, 73.399658],
+      "pictures": [
+        "https://scontent.cdninstagram.com/t51.2885-15/s320x320/e35/12142152_1510134265969536_6869965_n.jpg",
+        "http://twistedsifter.files.wordpress.com/2011/04/maldives-beaches-luxury-best-1.jpg?w=800&h=600",
+        "http://www.luxresorts.com/media/2601563/Maldives_Hotels_Resorts_LUX_Maldives_Spa_WellBeing.jpg",
+        "https://media-cdn.tripadvisor.com/media/photo-s/03/9b/2f/ce/maldives.jpg",
+        "https://www.maldives.com/wp-content/uploads/2015/07/Velaa-Private-Island-Maldives.jpg",
+      ]
+    },
+    {
+      "cover": "https://brightcove04pmdo-a.akamaihd.net/5104226627001/5104226627001_5297426881001_5280178297001-vs.jpg?pubId=5104226627001&videoId=5280178297001",
+      "location": "New Zealand",
+      "user": "Evan",
+      "crd": [-41.28664, 174.77557]
+    },
+    {
+      "cover": "https://s-media-cache-ak0.pinimg.com/originals/b2/4c/35/b24c35c4c1995fbe44d2459d6aa11506.jpg",
+      "location": "Patagonia",
+      "user": "Matt",
+      "crd": [-41.810147, -68.906269]
+    }
+  ],
+}
