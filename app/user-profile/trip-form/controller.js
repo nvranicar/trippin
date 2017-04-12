fd74ae5f296909ca02981a73966d6abe01c5a860
Evan Engels <evan.engels@gmail.com>
2017-04-12 13:38:19 -0500

    functioning calendar

--- app/user-profile/trip-form/controller.js
+++ app/user-profile/trip-form/controller.js
@@ -3,15 +3,35 @@ import Controller from 'ember-controller';
 import moment from 'moment';
 
 export default Ember.Controller.extend({
+  filesystem: Ember.inject.service(),
+  selectedFiles: [],
   formValues: {},
 
   actions: {
+    upload(file) {
+      const fetch = this.get('filesystem.fetch');
+
+      fetch('#', {
+        method: 'POST',
+        headers: {
+          accept: 'application/json'
+        },
+        body: {
+          'profile-image': file[0]
+        }
+      }).then(res => res.json()).then((data) => {
+        const upload = this.store.pushPayload(data);
+      });
+    },
+
     async saveTrip() {
       const trip = this.store.createRecord('trip', this.formValues);
-
       await trip.save();
-
       this.transitionToRoute('/user-profile.index');
+    },
+
+    changeMe(d) {
+      debugger;
     }
   }
 });
