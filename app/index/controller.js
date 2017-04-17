a19f6a6d22734e564edfde2439fb13a85614ae81
Nathan Vranicar <nathan@Nathans-MacBook-Pro-4.local>
2017-04-17 09:46:46 -0500

    added modal for pictures

--- app/index/controller.js
+++ app/index/controller.js
@@ -7,18 +7,26 @@ export default Ember.Controller.extend({
   lat: 38.82259097617713,
   lng: 9.140625000000002,
   zoom: 2,
-  ironYard: [36.152706, -86.776111],
-  bounds: [[-89.98155760646617, -200], [89.99346179538875, 200]],
+  ironYard: [
+    36.152706, -86.776111
+  ],
+  bounds: [
+    [
+      -89.98155760646617, -200
+    ],
+    [89.99346179538875, 200]
+  ],
   currentUser: {
-      favorites: []
+    favorites: []
   },
+  isShowingModal: false,
 
   actions: {
-     zoomByLoc(trip) {
-       Ember.set(this, 'lat', trip.crd[0]);
-       Ember.set(this, 'lng', trip.crd[1]);
-       Ember.set(this, 'zoom', 5);
-       this.get('scroller').scrollVertical('.map');
+    zoomByLoc(trip) {
+      Ember.set(this, 'lat', trip.crd[0]);
+      Ember.set(this, 'lng', trip.crd[1]);
+      Ember.set(this, 'zoom', 5);
+      this.get('scroller').scrollVertical('.map');
     },
     scroll(trip) {
       this.get('scroller').scrollVertical(`.${trip.id}`);
@@ -30,5 +38,13 @@ export default Ember.Controller.extend({
         this.currentUser.favorites.pushObject(pic);
       }
     },
+    toggleModal() {
+      this.toggleProperty('isShowingModal');
+      if (this.isShowingModal) {
+        document.querySelector('body').className = 'stop-scrolling';
+      } else {
+        document.querySelector('body').className = document.querySelector('body').className.replace(/(?:^|\s)stop-scrolling(?!\S)/g, '')
+      }
+    }
   }
 });
