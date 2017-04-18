437aa99c46fc5a89d35f3ae8b45470996a0bbd53
Nathan Vranicar <nathan@Nathans-MacBook-Pro-4.local>
2017-04-18 13:33:15 -0500

    added some ember data models and routes

--- app/models/location.js
+++ app/models/location.js
@@ -2,6 +2,5 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  locations: DS.hasMany('location'),
-  pictures: DS.hasMany('picture'),
+  trips: DS.hasMany('trip'),
 });
